9248bba62406ecdbe092ed6d7f6ed972f4094e5c
Ramzi Abdoch <ramzi_abdoch@mckinsey.com>
2017-07-29 17:20:20 -0700

    Initial Commit

---
+++ steps_of_the_forest_godTests/steps_of_the_forest_godTests.swift
@@ -0,0 +1,36 @@
+//
+//  steps_of_the_forest_godTests.swift
+//  steps_of_the_forest_godTests
+//
+//  Created by Ramzi Abdoch on 7/29/17.
+//  Copyright © 2017 Ramzi Abdoch. All rights reserved.
+//
+
+import XCTest
+@testable import steps_of_the_forest_god
+
+class steps_of_the_forest_godTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
